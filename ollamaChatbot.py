cadd62b83ef02da5aa142c9b8a1e835decec5978
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-05-23 21:34:13 +0530

    Add files via upload

---
+++ ollamaChatbot.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]="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"
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+## streamlit framework
+st.title('Langchain Demo With LLAMA2 API')
+input_text=st.text_input("Search the topic u want")
+ollama LLAma2 LLm 
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text: 
+    
+        st.write(chain.invoke({"question":input_text}))
